9d3077e88d961236e799f9c54e609500bfc46052
mr-vishwakarma <mayurvish2003@gmail.com>
2026-05-15 18:53:52 +0530

    Auth route Working

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
-app.use(cors());
+const authRoute = require("./routes/auth.routes")
 
+
+app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
