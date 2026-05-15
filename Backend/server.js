687a5c80659e0059e3593545e3bccae3f35fa730
mr-vishwakarma <mayurvish2003@gmail.com>
2026-05-15 18:23:53 +0530

    Setup Mongo database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const app = require("./src/app");
 
 const PORT = process.env.PORT || 4000;
+const connectDB = require("./src/config/db");
+connectDB();
+
 
 app.listen(PORT, () => {
   console.log(`Server running onport ${PORT}`);
