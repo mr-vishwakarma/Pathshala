3aa67111f863838bb9f14cf68b66b5e21dc464d9
mr-vishwakarma <mayurvish2003@gmail.com>
2026-05-15 20:02:23 +0530

    Create user registration System

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,15 +1,36 @@
 const User = require('../models/user.model');
+const bcrypt = require("bcryptjs");
 
 const registerUser = async(req, res) => {
     try {
+        const {fullname, email, password} = req.body; 
+
+        const existingUser = await User.findOne({ 
+            email,
+        })
+
+        if(existingUser) {
+            return res.status(400).json({
+                message:"User already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await User.create({
+            fullname,
+            email,
+            password: hashedPassword,
+        });
+
         res.status(201).json({
-            message:"Register Route working",
+            message:"User created Sucessfully",
         })
         
     } catch (error) {
         res.status(500).json({
             message:error.message,
-        }) 
+        });
     }
 }
 
