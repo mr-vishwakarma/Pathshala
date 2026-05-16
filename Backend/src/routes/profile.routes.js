b3f3066596df985f247aefbe5c2b126cbc1a44f9
mr-vishwakarma <mayurvish2003@gmail.com>
2026-05-16 15:50:17 +0530

    add profile routes

--- Backend/src/routes/profile.routes.js
+++ Backend/src/routes/profile.routes.js
@@ -4,13 +4,26 @@ const router = express.Router();
 
 const authMiddleware = require("../middleware/auth.middleware");
 
+const upload = require("../utils/multer");
+
 const {
   getProfile,
   updateProfile,
+  changePassword,
+  uploadProfilePhoto,
 } = require("../controllers/profile.controller");
 
 router.get("/me", authMiddleware, getProfile);
 
 router.put("/update", authMiddleware, updateProfile);
+
+router.put("/change-password", authMiddleware, changePassword);
+
+router.post(
+  "/upload-photo",
+  authMiddleware,
+  upload.single("image"),
+  uploadProfilePhoto,
+);
 
 module.exports = router;
