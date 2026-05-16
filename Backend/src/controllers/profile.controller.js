97f2551f5e7ce27f1d4f8883a1711cfd7465976c
mr-vishwakarma <mayurvish2003@gmail.com>
2026-05-16 15:50:06 +0530

    add profile work

--- Backend/src/controllers/profile.controller.js
+++ Backend/src/controllers/profile.controller.js
@@ -1,4 +1,7 @@
+const uploadToImageKit = require("../utils/uploadToImageKit");
+
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 const getProfile = async (req, res) => {
   try {
@@ -40,7 +43,67 @@ const updateProfile = async (req, res) => {
   }
 };
 
+const changePassword = async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+
+    const user = await User.findById(req.user._id);
+
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Old password is incorrect",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashedPassword;
+
+    await user.save();
+
+    res.status(200).json({
+      message: "Password changed successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const uploadProfilePhoto = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Image is required",
+      });
+    }
+
+    const uploadedImage = await uploadToImageKit(req.file);
+
+    const user = await User.findById(req.user._id);
+
+    user.profilePhoto = uploadedImage.url;
+
+    await user.save();
+
+    res.status(200).json({
+      message: "Profile photo uploaded successfully",
+
+      profilePhoto: uploadedImage.url,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getProfile,
   updateProfile,
+  changePassword,
+  uploadProfilePhoto,
 };
