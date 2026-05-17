2a2b5b32659d890135f62822e251ffee05547cd9
mr-vishwakarma <mayurvish2003@gmail.com>
2026-05-17 12:12:13 +0530

    project ininitialised

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import ReactDOM from "react-dom/client";
+
+import { RouterProvider } from "react-router-dom";
+
+import router from "./routes/router";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
 );
